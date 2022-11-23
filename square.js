1040069df1139d328a244be194b48323acf1ae58
qivvoon <gangjiwon70@gmail.com>
2022-11-23 17:29:48 +0900

    test(#44): mocha test for lunch.js and square.js
    mocha test를 할 수 있도록 lunch.js, square.js, test.spec.js를 변경.

--- square.js
+++ square.js
@@ -1,7 +1,13 @@
 const square = function (rtm, text, channel) {
-  console.log('제곱을 실시합니다.');
-  console.log(text);
-  rtm.sendMessage(`The result is ${text * text}`, channel);
+  console.log('제곱을 실시합니다');
+
+  try {
+    rtm.sendMessage(`The result is ${text * text}`, channel);
+    return Promise.resolve('success');
+  } catch (error) {
+    console.log('error!', error.data);
+    return Promise.resolve('error');
+  }
 };
 
 module.exports = square;
